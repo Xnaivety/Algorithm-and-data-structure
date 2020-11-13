c983d3854b14e214e7e9a071437ea838d89d1c08
Xnaivety <827725022@qq.com>
2020-11-13 19:30:35 +0800

    record

---
+++ 5、tree and binary tree/binary sort tree insertion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+typedef struct binode {
+    binode *lchild, *rchild;
+    int data;
+} binode, *bitree;
+
+//二叉排序树插入操作的算法描述
+int bst_insert(bitree &T, int n) {
+    if (T == NULL) {
+        T = (bitree) malloc(sizeof(binode));
+        T->lchild = T->rchild = NULL;
+        T->data = n;
+        return 1;//插入成功
+    } else {
+        if (T->data < n) {
+            bst_insert(T->rchild, n);
+        } else if (T->data > n) {
+            bst_insert(T->lchild, n);
+        } else if (T->data == n) {//遇到相同的结点
+            return 0;//返回0插入失败
+        }
+    }
+}
